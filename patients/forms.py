1f41db3c1cc2949ee7131166486509e2af447233
devalsofa <felipe.deval@sofadigital.com>
2023-03-23 14:07:42 -0300

    Bootstrap applyed

--- patients/forms.py
+++ patients/forms.py
@@ -1,7 +1,54 @@
+import pycountry
 from django import forms
 from .models import Patient
 
 class PatientForm(forms.ModelForm):
+    STATES = [
+        ('AC', 'Acre'),
+        ('AL', 'Alagoas'),
+        ('AP', 'Amapá'),
+        ('AM', 'Amazonas'),
+        ('BA', 'Bahia'),
+        ('CE', 'Ceará'),
+        ('DF', 'Distrito Federal'),
+        ('ES', 'Espírito Santo'),
+        ('GO', 'Goiás'),
+        ('MA', 'Maranhão'),
+        ('MT', 'Mato Grosso'),
+        ('MS', 'Mato Grosso do Sul'),
+        ('MG', 'Minas Gerais'),
+        ('PA', 'Pará'),
+        ('PB', 'Paraíba'),
+        ('PR', 'Paraná'),
+        ('PE', 'Pernambuco'),
+        ('PI', 'Piauí'),
+        ('RJ', 'Rio de Janeiro'),
+        ('RN', 'Rio Grande do Norte'),
+        ('RS', 'Rio Grande do Sul'),
+        ('RO', 'Rondônia'),
+        ('RR', 'Roraima'),
+        ('SC', 'Santa Catarina'),
+        ('SP', 'São Paulo'),
+        ('SE', 'Sergipe'),
+        ('TO', 'Tocantins')
+    ]
+
+    COUNTRY_CHOICES = [(country.alpha_2, country.name) for country in pycountry.countries]
+    
+    name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    surname = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    phone = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    date_of_birth = forms.DateField(widget=forms.DateInput(attrs={'class': 'form-control'}))
+    age = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    weight = forms.DecimalField(widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    height = forms.DecimalField(widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    address = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    cep = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    city = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    state = forms.ChoiceField(choices=STATES, widget=forms.Select(attrs={'class': 'form-control'}))
+    country = forms.ChoiceField(choices=COUNTRY_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
+    
     class Meta:
         model = Patient
-        fields = '__all__'
+        fields = ['name', 'surname', 'email', 'phone', 'date_of_birth', 'age', 'weight', 'height', 'address', 'cep', 'city', 'state', 'country']
--- patients/models.py
+++ patients/models.py
@@ -1,14 +1,53 @@
+import pycountry
 from django.db import models
 
+
 class Patient(models.Model):
+    COUNTRY_CHOICES = [(country.alpha_2, country.name) for country in pycountry.countries]
+
+    STATES = [
+        ('AC', 'Acre'),
+        ('AL', 'Alagoas'),
+        ('AP', 'Amapá'),
+        ('AM', 'Amazonas'),
+        ('BA', 'Bahia'),
+        ('CE', 'Ceará'),
+        ('DF', 'Distrito Federal'),
+        ('ES', 'Espírito Santo'),
+        ('GO', 'Goiás'),
+        ('MA', 'Maranhão'),
+        ('MT', 'Mato Grosso'),
+        ('MS', 'Mato Grosso do Sul'),
+        ('MG', 'Minas Gerais'),
+        ('PA', 'Pará'),
+        ('PB', 'Paraíba'),
+        ('PR', 'Paraná'),
+        ('PE', 'Pernambuco'),
+        ('PI', 'Piauí'),
+        ('RJ', 'Rio de Janeiro'),
+        ('RN', 'Rio Grande do Norte'),
+        ('RS', 'Rio Grande do Sul'),
+        ('RO', 'Rondônia'),
+        ('RR', 'Roraima'),
+        ('SC', 'Santa Catarina'),
+        ('SP', 'São Paulo'),
+        ('SE', 'Sergipe'),
+        ('TO', 'Tocantins')
+    ]
+
+
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, default='default_value')
+    phone = models.CharField(max_length=20)
     date_of_birth = models.DateField()
-    age = models.PositiveIntegerField()
+    age = models.IntegerField()
     weight = models.DecimalField(max_digits=5, decimal_places=2)
     height = models.DecimalField(max_digits=5, decimal_places=2)
     address = models.CharField(max_length=200)
-    cep = models.CharField(max_length=9)
+    cep = models.CharField(max_length=8)
     city = models.CharField(max_length=100)
-    country = models.CharField(max_length=100)
-    phone = models.CharField(max_length=20)
+    state = models.CharField(max_length=2, choices=STATES)
+    country = models.CharField(max_length=2, choices=COUNTRY_CHOICES)
+
+    
